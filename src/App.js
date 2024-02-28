861019f5bd6dac9b67468d1308471a0fff47e0e8
Gomes V <ggvgabriel05@gmail.com>
2024-02-28 11:20:52 -0300

    PDF created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import { PDFViewer } from "@react-pdf/renderer";
+import Invoice from "./Invoice";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <PDFViewer width="100%" height="100%" className="app" >
+        <Invoice />
+      </PDFViewer>
     </div>
   );
 }
